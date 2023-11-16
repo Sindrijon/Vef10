0e5df06c1937c36474ea8b4a2bd52627c8e8ac78
Sindrijon <sindrijons@gmail.com>
2023-11-16 13:14:02 +0000

    verkefni 10

---
+++ v10/src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { useRef, useEffect } from 'react';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+      <Profile />
+      <VideoComponent />      
+      </header>
+    </div>
+  );
+}
+
+
+
+function VideoComponent() {
+  const videoRef = useRef(null);
+
+  const togglePlayPause = () => {
+    const video = videoRef.current;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  };
+
+  return (
+    <div>
+      <video ref={videoRef} src="HolyCow.mp4" width="320" height="240" controls></video>
+      <button onClick={togglePlayPause}>Play/Pause</button>
+    </div>
+  );
+}
+
+const user = {
+  name: 'Árni Bald',
+  imageUrl: "arni.png",
+  imageSize: 150,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+
+export default App
